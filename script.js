2a7f5524cf3a0fe1acae1ed97a024508d752001d
Tom Erik Wiklund <tomerikwiklund@gmail.com>
2020-05-24 19:06:00 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+// question 1
+
+//
+var firstVar;
+
+// question 2
+
+//
+var name = "Tom";
+
+// question 3
+
+//
+var age = 5;
+
+// question 4
+
+//
+var division = 20 / 5;
+
+// question 5
+
+//
+var animal = "frog";
+typeof animal;
+typeof "frog";
+
+// question 6
+
+//
+var orderHasShipped = true;
+
+// question 7
+
+//
+var orderHasShipped = true;
+
+if (orderHasShipped === true) {
+  console.log("Order has shipped");
+} else {
+  console.log("Order is NOT shipped");
+}
+
+// question 8
+
+//
+for (var count = 1; count <= 9; count++) {
+  console.log(count);
+}
